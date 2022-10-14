b31b167eccf034491e6f0a7a7b46a75670ca3957
giraffesarecool1 <73612023+giraffesarecool1@users.noreply.github.com>
2022-10-14 05:02:29 -0400

    added some components

---
+++ src/aboutSection.jsx
@@ -0,0 +1,24 @@
+/*import profilePic from "./profilePic.jpg"
+*/
+
+const aboutSection = () => {
+    return (
+        <div class="flexwrapper">
+            <h2>About</h2>
+            <div>
+                <img src="#" alt="#" />
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+            <div id="flexwrapper">
+            <img src="https://benscott.dev/imgs/js.f8a28e905c78dadb79434b7ceebd52a0.png" />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg" />
+            <img src="https://benscott.dev/imgs/react.cb15bfce2a9004ce61c5f79f805b067b.png" />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg" />
+            <img src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.svg" />
+            
+            </div>
+        </div>
+    )
+}
+
+export default aboutSection;
---
+++ src/navBar.jsx
@@ -0,0 +1,14 @@
+const navBar = () => {
+    return (
+        <nav class="navbar">
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects.asp">Projects</a></li>
+                <li><a href="#contact.asp">Contact</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default navBar;
