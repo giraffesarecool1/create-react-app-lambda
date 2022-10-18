ea4f9591d09a097b727c06b73f1e3af7aba048d6
giraffesarecool1 <73612023+giraffesarecool1@users.noreply.github.com>
2022-10-18 02:58:44 -0400

    exodia

---
+++ src/contactSection.jsx
@@ -0,0 +1,15 @@
+const contactSection = () => {
+    return (
+        <div class="flexwrapper">
+            <h2>Contact</h2>
+            <p>Please don't hesitate to reach out. Simply leave a message and I'll get back to you as soon as possible.</p>
+            <form>
+            <input name="name" placeholder="Name" />
+            <input type="email" name="_replyto" placeholder="Email" />
+            <textarea name="message" placeholder="Your Message Here"></textarea>
+            </form>
+        </div>
+    )
+}
+
+export default contactSection;
---
+++ src/projectsSection.jsx
@@ -0,0 +1,20 @@
+import crimeMap from "./crimeMap.jpg";
+import podcastPlanners from "./podcastPlanners.jpg";
+
+const projectsSection = () => {
+    return (
+        <div class="flexwrapper">
+            <h2>Projects</h2>
+            <div>
+                <a href="https://nifty-blackwell-b8fba1.netlify.app/" ><img src={crimeMap} /></a>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+            <div>
+                <a href="https://main--curious-bunny-7f4ff1.netlify.app/" ><img src={podcastPlanners} /></a>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+        </div>
+    )
+}
+
+export default projectsSection;
