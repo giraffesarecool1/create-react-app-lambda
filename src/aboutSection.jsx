cd3d796335a4e3aa183b1ad83faa8ed1d74fb509
giraffesarecool1 <73612023+giraffesarecool1@users.noreply.github.com>
2022-11-01 09:44:21 -0400

    styles

--- src/aboutSection.jsx
+++ src/aboutSection.jsx
@@ -5,10 +5,10 @@
 import React, { Component }  from 'react';
 const AboutSection = () => {
     return (
-        <div class="flexwrapper">
+        <div class="flexwrapper AboutSection">
             <h2>About</h2>
             <div>
-                <img src="#" alt="#" />
+                <img src="https://pmsol3.files.wordpress.com/2009/08/chineseaverageman.jpg" alt="#" id="me"/>
                 <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
             </div>
             <div id="flexwrapper">
